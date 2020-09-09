89e64a3dd27b466326b015f85c3b0441bd7c6bb9
長谷川優太 <hasegawayuta@Hasex.local>
2020-09-09 23:51:08 +0900

    add fromJson

--- mobile_app/app/lib/models/todo.dart
+++ mobile_app/app/lib/models/todo.dart
@@ -4,4 +4,13 @@ class Todo {
   final String description;
 
   Todo({this.id, this.title, this.description});
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+    );
+  }
+
 }
