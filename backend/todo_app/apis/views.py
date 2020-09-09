8e128939b82972a6928f21b45858f10b68c95f4e
長谷川優太 <hasegawayuta@Hasex.local>
2020-09-09 22:26:01 +0900

    add api views

--- backend/todo_app/apis/views.py
+++ backend/todo_app/apis/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from todos import models
+from .serializers import TodoSerializer
+
+class ListTodo(generics.ListCreateAPIView):
+	queryset = models.Todo.object.all()
+	serializer_class = TodoSetialize
+
+class DerailTodo(generics.RetriveUpdateDestroyAPIView):
+	queryset = models.Todo.object.all()
+	serializer_class = TodoSetialize
+
