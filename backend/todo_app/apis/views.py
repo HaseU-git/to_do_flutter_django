5e07e66bc897169a29c66ee7ab3b920f73ebd5ce
長谷川優太 <hasegawayuta@Hasex.local>
2020-09-09 22:39:11 +0900

    fix object to objects

--- backend/todo_app/apis/views.py
+++ backend/todo_app/apis/views.py
@@ -6,10 +6,10 @@ from todos import models
 from .serializers import TodoSerializer
 
 class ListTodo(generics.ListCreateAPIView):
-	queryset = models.Todo.object.all()
+	queryset = models.Todo.objects.all()
 	serializer_class = TodoSetialize
 
 class DerailTodo(generics.RetriveUpdateDestroyAPIView):
-	queryset = models.Todo.object.all()
+	queryset = models.Todo.objects.all()
 	serializer_class = TodoSetialize
 
